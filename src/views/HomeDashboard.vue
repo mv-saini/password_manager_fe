758460f4d5dd2376096a152ae30a46baf4a99925
Mohit Vijay Saini <snamvs75@gmail.com>
2023-04-02 11:39:26 +0200

    added optional tab

--- src/views/HomeDashboard.vue
+++ src/views/HomeDashboard.vue
@@ -2,10 +2,16 @@
     import { ref, watch } from 'vue';
     import AppLogo from '@/components/AppLogo.vue';
     var tab = ref(null)
+    var tabH = ref(null)
     var length = ref(0)
+    var lengthH = ref(0)
 
     watch(length, (val) => {
         tab.value = val
+    })
+
+    watch(lengthH, (val) => {
+        tabH.value = val
     })
     
 </script>
@@ -54,6 +60,58 @@
                         </v-window>
                     </div>
                 </v-card>
+            </v-col>
+            <v-col cols="2"/>
+        </v-row>
+    </v-container>
+
+    <v-container>
+        <v-row>
+            <v-col cols="2"/>
+            <v-col cols="8">
+                <v-toolbar color="primary">
+                        <v-toolbar-title>Dashboard</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <v-btn icon @click="lengthH++">
+                            <v-icon>mdi-plus</v-icon>
+                        </v-btn>
+                        <v-btn icon>
+                            <v-icon>mdi-dots-vertical</v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                <v-card>
+                    <v-tabs
+                    v-model="tabH"
+                    bg-color="teal-darken-3"
+                    show-arrows
+                    slider-color="teal-lighten-3"
+                    >
+                    <v-tab
+                        v-for="i in lengthH"
+                        :key="i"
+                        :value="'tab-' + i"
+                    >
+                        Item {{ i }}
+                    </v-tab>
+                    </v-tabs>
+                </v-card>
+
+                <v-card-text>
+                    <v-window v-model="tabH">
+                            <v-window-item
+                            v-for="i in lengthH"
+                        :key="i"
+                        :value="'tab-' + i">
+                                <v-card flat>
+                                    <v-card-text>
+                                        <p>
+                                            Item text {{ i }}
+                                        </p>
+                                    </v-card-text>
+                                </v-card>
+                            </v-window-item>
+                        </v-window>
+                </v-card-text>
             </v-col>
             <v-col cols="2"/>
         </v-row>
